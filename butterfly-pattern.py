2349f7313cdc36c6a3e01608171f56c988e12cbb
Subeg Singh Lamba <subegsingh831@gmail.com>
2026-07-06 20:35:45 +0530

    Clarify input value for number of rows
    Added a comment to clarify the value of n.

--- butterfly-pattern.py
+++ butterfly-pattern.py
@@ -8,7 +8,7 @@
 *       *
 '''
 
-n=int(input('enter the number of rows:'))
+n=int(input('enter the number of rows:')) #here n=4
 for i in range(1, n + 1):
     if i==n:
         print('*'*(2*n+1),end="")
