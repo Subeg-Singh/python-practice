820c0be95319f7472188a48c863044624a0a6630
Subeg Singh Lamba <subegsingh831@gmail.com>
2026-07-15 19:24:28 +0530

    Refactor guess_the_number game logic
    Fix invalid choice message and improve game logic.

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,20 +2,29 @@ import random
 a=random.randint(1,100)
 num=int(input('choose a number from 1 to 100:'))
 count=0
+
 def game(a,num,count):
+    
     while True:
+        
         if num==a:
             print('You guessed it right!')
             count+=1
             break
+            
         elif num<a:
             print(f'Choose a number bigger than {num} from 1 to 100')
             count+=1
             num=int(input('choose a number:'))
+            
         elif num>a:
             print(f'Choose a number smaller than {num} from 1 to 100')
             count+=1
             num=int(input('choose a number:'))
+            
+        else:
+            print('INVALID CHOICE!)
+                  
     print(f'you guess the number in {count} turns!')
 
 game(a,num,count)
